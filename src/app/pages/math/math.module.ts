f605b1edec6adbc29db2f22b9c3404eca8eda23b
Max Schweigerdt <max-schweigerdt@web.de>
2019-09-06 13:12:35 +0200

    rejuvenate

--- src/app/pages/math/math.module.ts
+++ src/app/pages/math/math.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { BusinessCardModule } from 'src/app/utils/business-card/business-card.module';
-import { DropNodesModule } from './garden/drops/drops.module';
+import { DropNodesModule } from './garden/drop/drop.module';
 import { GoldenModule } from './theorems/golden-ratio/golden-ratio.module';
 import { TheoremsModule } from './theorems/theorems.module';
 import { VelocityModule } from './velocity/velocity.module';
 import { IsometricModule } from './isometric/isometric.module';
-import { NodeGardenModule } from './garden/nodes/node.module';
+import { NodeGardenModule } from './garden/node/node.module';
 
 @NgModule({
   declarations: [
